9ae4f7c650545616a821b8ae2dcbc7da932ab57d
Sean Breckenridge <seanbrecke@gmail.com>
2019-01-13 03:23:02 -0800

    add atom config

---
+++ atom/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# .installed is an empty file to represent that atom
+# has already been symlinked
+if [ ! -f ${DOTDIR}/atom/.installed ]; then
+  rm -rf ~/.atom
+  ln -s ${DOTDIR}/atom/atom/ ~/.atom
+  touch ${DOTDIR}/atom/.installed
+  echo "› symlinked atom directory"
+else
+  echo "› atom has already been symlinked"
+fi
+  
