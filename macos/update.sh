7a59b8ad29d09f40ca7c974a0f99a22715711fd5
Sean Breckenridge <seanbrecke@gmail.com>
2019-01-19 15:32:07 -0800

    add a timeout to macos updates

--- macos/update.sh
+++ macos/update.sh
@@ -3,5 +3,21 @@
 # command line interface to install updates from the Mac App Store
 
 set -e
-echo "› sudo softwareupdate -i -a"
-sudo softwareupdate -i -a
+
+if [[ $(uname -s) == "Darwin" ]]; then
+  echo -e "› macos/update.sh installs update from the Mac App Store."
+  printf "› install updates? "
+  read -t 15 resp # timeout after 15 seconds
+  case "$resp" in
+    y|Y )
+      echo "› sudo softwareupdate -i -a"
+      sudo softwareupdate -i -a
+      ;;
+    n|N ) ;;
+    * ) printf "Invalid response (%s). Run 'set_mac_defaults' and use (y/n)" $resp
+  esac
+else
+  echo -e "› Not on macOS, skipping macos"
+fi
+
+
