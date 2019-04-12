2004637c03e8e410a2ab0e8508ba2ea3209926d7
Sean Breckenridge <seanbrecke@gmail.com>
2019-04-11 19:25:53 -0700

    add command to remove speedtest-cli wrapper if speedtest-cli is updated

--- python3/install.sh
+++ python3/install.sh
@@ -4,3 +4,10 @@ echo -e "› Upgading pip"
 pip3 install --upgrade --user pip setuptools wheel # upgrade pip
 echo -e "› Installing/Upgrading packages"
 pip3 install --upgrade --user -r "${DOTDIR}/python3/requirements3.txt"
+
+# speedtest installs 2 executable scripts
+# speedtest and speedtest-cli, which both have the same
+# functionatly. This removes speedtest-cli to make
+# tab completion nicer
+[[ ! $(command -v speedtest-cli | wc -c | xargs echo) == "0" ]] && rm `which speedtest-cli`
+# xargs echo removes the extra spaces wc prepends to ouput
