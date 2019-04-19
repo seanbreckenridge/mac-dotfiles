eb82fb26f3e0446c6a286fa2c563f35bc12d6e20
Sean Breckenridge <seanbrecke@gmail.com>
2019-04-18 17:36:00 -0700

    use 'command' exit code instead of weird parsing with wc/xargs

--- python3/install.sh
+++ python3/install.sh
@@ -9,5 +9,4 @@ pip3 install --upgrade --user -r "${DOTDIR}/python3/requirements3.txt"
 # speedtest and speedtest-cli, which both have the same
 # functionatly. This removes speedtest-cli to make
 # tab completion nicer
-[[ ! $(command -v speedtest-cli | wc -c | xargs echo) == "0" ]] && rm `which speedtest-cli`
-# xargs echo removes the extra spaces wc prepends to ouput
+[[ $(command -v speedtest-cli) ]] && rm `which speedtest-cli`
