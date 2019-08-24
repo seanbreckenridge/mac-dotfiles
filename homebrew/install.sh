6aa2cfc0dd710c45278b4c87512d7e4f5b6381e4
Sean Breckenridge <seanbrecke@gmail.com>
2019-08-23 22:21:00 -0700

    called with bash -x, echo isnt needed

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -12,8 +12,6 @@
 # Check for Homebrew
 if test ! $(which brew)
 then
-  echo "  Installing Homebrew for you."
-
   # Install the correct homebrew for each OS type
   if test "$(uname)" = "Darwin"
   then
